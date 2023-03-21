5550a280ebf541f6b0e81200ccce4a465857ecc3
phoreverpheebs <phoreverphoebs@proton.me>
2023-03-22 06:41:48 +0900

    int 0x80

--- src/main.rs
+++ src/main.rs
@@ -7,23 +7,19 @@ use std::{
 const HAI: &str = "hai";
 
 fn main() {
-    let volebx = HAI.as_ptr(); // the first variable declared is in ebx
-
     let scfunc = unsafe {
-        let mut addr = mem::transmute::<fn(u32, u32) -> u32, *mut u8>(sc);
-        while ptr::read(addr) != 0x0f {
+        let mut addr = mem::transmute::<fn(*mut u32, u32, u32, u32, &mut u8) -> u32, *mut u8>(int80);
+        while ptr::read(addr) != 0x97 {
             addr = (addr as usize + 1) as *mut u8;
         }
-        mem::transmute::<usize, fn(usize) -> usize>(addr as usize + 3)
+        mem::transmute::<*mut u8, fn(usize) -> usize>(addr)
     };
 
-    not_syscall(1, 0, 1, 1, scfunc);
+    let hai = unsafe {
+        mem::transmute::<&str, (usize, usize)>(HAI).0
+    };
 
-    _ = volebx;
-}
-
-fn void(a: usize) -> usize {
-    a
+    not_syscall(4, 0, 1, hai, scfunc);
 }
 
 #[inline(never)]
@@ -35,12 +31,17 @@ fn not_syscall(
     sc: fn(usize) -> usize,
 ) -> usize {
     sc(eax);
-    eax
+    edx + ecx
 }
 
-#[inline(never)]
-fn sc(a: u32, b: u32) -> u32 {
-    let mut r = a + b;
-    r &= 0xf;
-    r + 0xfaeb97c3
+pub fn int80(a: *mut u32, _b: u32, c: u32, d: u32, x: &mut u8) -> u32 {
+    unsafe { *((a as usize + 0x05eb) as *mut u32) = c; }
+    *x = 0xcd; // won't be optimised away
+    let mut d = d as u8; /* makes sense to use the variable in ecx, because modr/m will give 0xf9
+    which is a legal instruction for user-space, whereas using the variable in edx (c: u32) will give
+    0xfa modr/m (cli), which would raise a #GP(0) */
+    while d < 0xc2 { // an 8-bit comparison, to get an early ret
+        d += 1;
+    }
+    d as u32
 }
