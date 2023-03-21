61bb07f30218e86b61c154cc7d021ef135bc630b
phoreverpheebs <phoreverphoebs@proton.me>
2023-03-21 18:39:53 +0900

    I don't understand x64 syscalls

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,46 @@
-use std::mem;
+#![allow(dead_code, unused_variables)]
+use std::{
+    // arch::asm,
+    mem, ptr,
+};
+
+const HAI: &str = "hai";
 
 fn main() {
-    let adder: fn(i32, i32) -> i32 = unsafe { 
-        mem::transmute::<usize, fn(i32, i32) -> i32>(
-            mem::transmute::<fn(i32, i32) -> i32, usize>(add) + 3)
+    let volebx = HAI.as_ptr(); // the first variable declared is in ebx
+
+    let scfunc = unsafe {
+        let mut addr = mem::transmute::<fn(u32, u32) -> u32, *mut u8>(sc);
+        while ptr::read(addr) != 0x0f {
+            addr = (addr as usize + 1) as *mut u8;
+        }
+        mem::transmute::<usize, fn(usize) -> usize>(addr as usize + 3)
     };
-    println!("0x{:x}", adder(5, 5));
+
+    not_syscall(1, 0, 1, 1, scfunc);
+
+    _ = volebx;
 }
 
-fn add(a: i32, b: i32) -> i32 {
-    a + b
+fn void(a: usize) -> usize {
+    a
+}
+
+#[inline(never)]
+fn not_syscall(
+    eax: usize, 
+    _ebx: usize, 
+    edx: usize,
+    ecx: usize, 
+    sc: fn(usize) -> usize,
+) -> usize {
+    sc(eax);
+    eax
+}
+
+#[inline(never)]
+fn sc(a: u32, b: u32) -> u32 {
+    let mut r = a + b;
+    r &= 0xf;
+    r + 0xfaeb97c3
 }
